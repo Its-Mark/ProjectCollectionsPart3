4cc937891a54e15eabae98fc18624a62d3fd321c
Mark G <milk8001@hotmail.com>
2020-04-08 16:27:18 -0700

    finished program. just need to finish conclusion

--- src/Part3.java
+++ src/Part3.java
@@ -4,13 +4,14 @@
  *         mark.garcia01@student.csulb.edu
  * @author Brandon Wiitanen
  *         brandon.wiitanen01@student.csulb.edu
+ * Nanoseconds it took using ArrayList: 5194595700
+ * Nanoseconds it took using LinkedList: 2993986100
+ * In conclusion, the most efficient type of list to use in this program would be a LinkedList.
  */
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.LinkedList;
+import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+
 /**
  * A Scavenger Hunt is a party game that people play in teams. They are given a list of items to find throughout the surrounding neighborhood. Our challenge is to find the best data structure to store the results of the hunt.
  * Create a list of 100 items that you might have people find on the hunt. These should be items that they have a chance of finding through out the neighboorhood. For example, yesterday's newspaper, a shoelace, a paperbag,.... Load the list anyway you want (console input, flatfile, etc.)
@@ -27,7 +28,7 @@ import java.util.Scanner;
  */
 public class Part3 {
     public static void main(String[] args){
-        Scanner numGroups = new Scanner(System.in);
+        Scanner numTeams = new Scanner(System.in);
         Scanner elementIndex = new Scanner(System.in);
         ArrayList<String> aL = new ArrayList<>();
         LinkedList<String> lL = new LinkedList<>();
@@ -36,16 +37,80 @@ public class Part3 {
             File items = new File("100items.txt");
             Scanner itemScanner = new Scanner(items);
             if(args[0].compareToIgnoreCase("arrayList") == 0){
-                while(itemScanner.hasNext()){
-                    aL.add(itemScanner.next());
+                ArrayList<ArrayList> teams = new ArrayList<>();
+                while(itemScanner.hasNextLine()){
+                    aL.add(itemScanner.nextLine());
                 }
-
+                System.out.println("Beginning to End: ");
+                for(int i = 0; i < aL.size(); i++){
+                    System.out.println(aL.get(i));
+                }
+                System.out.println("End to Beginning: ");
+                for(int j = aL.size() - 1; j >= 0; j--){
+                    System.out.println(aL.get(j));
+                }
+                System.out.println("Enter a number of teams to play: ");
+                int nT = numTeams.nextInt();
+                System.out.println("Creating " + nT + " teams...");
+                for(int k = 0; k < nT; k++){
+                    teams.add(new ArrayList(aL));
+                }
+                for(ArrayList a : teams){
+                    Collections.shuffle(a);
+                }
+                System.out.println("Enter an index of an element(0-99): ");
+                int eI = elementIndex.nextInt();
+                System.out.println("Element at that index: ");
+                for(ArrayList a : teams){
+                    System.out.println(a.get(eI));
+                }
+                for(ArrayList a : teams){
+                    a.add(eI, "rusty nail");
+                }
+                int randIndex = new Random().nextInt(100);
+                System.out.println("Element at random index (" + randIndex + "):");
+                for(ArrayList a : teams){
+                    System.out.println(a.get(randIndex));
+                }
                 long end = System.nanoTime();
                 System.out.println("The time (nanoseconds) it took to complete all the operations was: " + (end-start));
                 //end if
             } else if (args[0].compareToIgnoreCase("linkedlist") == 0){
-                while(itemScanner.hasNext()){
-                    lL.add(itemScanner.next());
+                ArrayList<LinkedList> teams = new ArrayList<>();
+                while(itemScanner.hasNextLine()){
+                    lL.add(itemScanner.nextLine());
+                }
+                System.out.println("Beginning to End: ");
+                for(int i = 0; i < lL.size(); i++){
+                    System.out.println(lL.get(i));
+                }
+                System.out.println("End to Beginning: ");
+                for(int j = lL.size() - 1; j >= 0; j--){
+                    System.out.println(lL.get(j));
+                }
+                System.out.println("Enter a number of teams to play: ");
+                int nT = numTeams.nextInt();
+                System.out.println("Creating " + nT + " teams...");
+                for(int k = 0; k < nT; k++){
+                    LinkedList<String> tempList = (LinkedList) lL.clone();
+                    teams.add(tempList);
+                }
+                for(LinkedList a : teams){
+                    Collections.shuffle(a);
+                }
+                System.out.println("Enter an index of an element(0-99): ");
+                int eI = elementIndex.nextInt();
+                System.out.println("Element at that index: ");
+                for(LinkedList a : teams){
+                    System.out.println(a.get(eI));
+                }
+                for(LinkedList a : teams){
+                    a.add(eI, "rusty nail");
+                }
+                int randIndex = new Random().nextInt(100);
+                System.out.println("Element at random index (" + randIndex + "):");
+                for(LinkedList a : teams){
+                    System.out.println(a.get(randIndex));
                 }
 
                 long end = System.nanoTime();
